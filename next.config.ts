b28c96ded4f8497fc272c1e0df1defc5887f90ba
jrsmth <jamesrichardsmith97@gmail.com>
2025-12-26 09:13:35 +0000

    [SKY-8] deploy to gh pages

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
   transpilePackages: ["@project-skymap/library"],
   experimental: {
     externalDir: true
